4a68ae0b7b3b9b472110c8779db2c0e75c56da64
ia.baranov <ia.baranov@jet.msk.su>
2017-05-10 16:01:54 +0300

    Added FOR loops

--- ie-regedit.bat
+++ ie-regedit.bat
@@ -1,15 +1,15 @@
 rem Internet Explorer 11 registry settings for Selenium support (32-bit only)
-
+rem -------------------------------------------------------------------------
 rem Disable "Protected Mode" For Each Zone
-FOR /L %%G IN (1,1,4) DO reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\%%G" /v "2500" /t "REG_DWORD" /d 0 /f
-rem Current User
-FOR /L %%G IN (1,1,4) DO reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\%%G" /v "2500" /t "REG_DWORD" /d 0 /f
+FOR %%D IN (HKLM, HKCU) DO (
+FOR /L %%G IN (1,1,4) DO (
+reg add "%%D\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\%%G" /v "2500" /t "REG_DWORD" /d 0 /f
+))
 
 rem Disable "Enhanced Protected Mode"
 reg add "HKLM\Software\Microsoft\Internet Explorer\Main" /v "Isolation" /d "PMIL" /f
-reg add "HKLM\Software\Wow6432Node\Microsoft\Internet Explorer\Main" /v "Isolation" /d "PMIL" /f
 
 rem To maintain connection to IE instance
-reg add "HKLM\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BFCACHE" /v "iexplore.exe" /t "REG_DWORD" /d 0 /f
-rem Current User
-reg add "HKCU\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BFCACHE" /v "iexplore.exe" /t "REG_DWORD" /d 0 /f
+FOR %%D IN (HKLM, HKCU) DO (
+reg add "%%D\SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BFCACHE" /v "iexplore.exe" /t "REG_DWORD" /d 0 /f
+)
